6acd4f17cf824fcbca24efeddacd00451d4edea6
Luz <valdeztorresluzandrea@gmail.com>
2025-09-02 15:31:34 -0500

    subiendo ejercicio1 - SEM1

---
+++ SEMANA1_PHYTON/SEMANA1_PHYTON.py
@@ -0,0 +1,7 @@
+def ejer1(): #creando metodo ejer1
+    nombre=input("Ingrese su nombre:")
+    carrera=input("Ingrese su carrera:")
+
+    print(f"\n{nombre}, bienevenido a FA de {carrera}")
+
+ejer1()
